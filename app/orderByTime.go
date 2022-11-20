a2632817806e152b8e65720290decc65ce27174a
zengchen1024 <chenzeng2@huawei.com>
2022-11-20 22:14:04 +0800

    tolerate the resource is missing

--- app/orderByTime.go
+++ app/orderByTime.go
@@ -13,17 +13,15 @@ type orderByTime struct {
 // sortAndSet will sort the items by time in descending order.
 // But, most of the elements in items are already sorted in ascending order.
 // In order to increase the sorting, no need to reverse the items.
-func sortAndSet(items []orderByTime, do func(int, int) error) error {
+func sortAndSet(items []orderByTime, do func(int) error) error {
 	sort.Slice(items, func(i, j int) bool {
 		return items[i].t < items[j].t
 	})
 
-	for i, j := len(items)-1, 0; i >= 0; i-- {
-		if err := do(items[i].p, j); err != nil {
+	for i := len(items) - 1; i >= 0; i-- {
+		if err := do(items[i].p); err != nil {
 			return err
 		}
-
-		j++
 	}
 
 	return nil
