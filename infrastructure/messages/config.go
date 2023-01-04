32ba7016ca767e48ae66bd8368f061bb89c844de
zengchen1024 <chenzeng2@huawei.com>
2023-01-04 14:52:54 +0800

    update config for messages

---
+++ infrastructure/messages/config.go
@@ -0,0 +1,15 @@
+package messages
+
+var topics Topics
+
+type Topics struct {
+	Like            string `json:"like"             required:"true"`
+	Fork            string `json:"fork"             required:"true"`
+	Training        string `json:"training"         required:"true"`
+	Finetune        string `json:"finetune"         required:"true"`
+	Following       string `json:"following"        required:"true"`
+	Inference       string `json:"inference"        required:"true"`
+	Evaluate        string `json:"evaluate"         required:"true"`
+	Submission      string `json:"submission"       required:"true"`
+	RelatedResource string `json:"related_resource" required:"true"`
+}
