6679b5caf2dd1a74c220c42685a94c03258c9f25
victorzhou123 <victorzhoux@163.com>
2023-07-15 11:20:16 +0800

    fix: get expiry time from config

--- common/infrastructure/redis/dao.go
+++ common/infrastructure/redis/dao.go
@@ -18,7 +18,7 @@ func NewDBRedis(expiration int) dbRedis {
 func (r dbRedis) Create(
 	ctx context.Context, key string, value interface{},
 ) *redis.StatusCmd {
-	return client.Set(ctx, key, value, 500*time.Second)		// TODO to config
+	return client.Set(ctx, key, value, r.Expiration*time.Second)
 }
 
 func (r dbRedis) Get(
@@ -36,7 +36,7 @@ func (r dbRedis) Delete(
 func (r dbRedis) Expire(
 	ctx context.Context, key string, expire time.Duration,
 ) *redis.BoolCmd {
-	return client.Expire(ctx, key, 3*time.Second)	// TODO to config
+	return client.Expire(ctx, key, 3*time.Second)
 }
 
 func DB() *redis.Client {
