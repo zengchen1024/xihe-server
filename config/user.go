ae04873b87e16e623ce374e0d0c8673ea805c6bb
lichaoran <pkwarcraft@gmail.com>
2023-09-22 16:05:39 +0800

    remove unused configs
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- config/user.go
+++ config/user.go
@@ -1,19 +1,15 @@
 package config
 
 import (
-	"github.com/opensourceways/xihe-server/infrastructure/authingimpl"
 	"github.com/opensourceways/xihe-server/user/infrastructure/messageadapter"
 )
 
 type userConfig struct {
-	authingimpl.Config
-
 	Message messageadapter.Config `json:"message"   required:"true"`
 }
 
 func (cfg *userConfig) ConfigItems() []interface{} {
 	return []interface{}{
-		&cfg.Config,
 		&cfg.Message,
 	}
 }
