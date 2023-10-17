aaca895643ffc4ba054589e096e16ddcddb5c776
fuyong <1277145053@qq.com>
2023-10-17 20:38:58 +0800

    update redis-lib add tls

--- common/infrastructure/redis/db.go
+++ common/infrastructure/redis/db.go
@@ -2,6 +2,10 @@ package redis
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"os"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -11,13 +15,33 @@ var (
 )
 
 func Init(cfg *Config) error {
+	ca, err := ioutil.ReadFile(cfg.DBCert)
+	if err != nil {
+		return err
+	}
+
+	if err := os.Remove(cfg.DBCert); err != nil {
+		return err
+	}
+
+	pool := x509.NewCertPool()
+	if ok := pool.AppendCertsFromPEM(ca); !ok {
+		return err
+	}
+
+	tlsConfig := &tls.Config{
+		RootCAs:            pool,
+		InsecureSkipVerify: true,
+	}
+
 	client = redis.NewClient(&redis.Options{
-		Addr:     cfg.Address,
-		Password: cfg.Password,
-		DB:       0,
+		Addr:      cfg.Address,
+		Password:  cfg.Password,
+		DB:        0,
+		TLSConfig: tlsConfig,
 	})
 
-	_, err := client.Ping(context.Background()).Result()
+	_, err = client.Ping(context.Background()).Result()
 	if err != nil {
 		return err
 	}
